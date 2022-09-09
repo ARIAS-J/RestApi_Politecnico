a324cc057e90b35474ff8563112f172c4c552d8b
No-Hex <evilheroe1@gmail.com>
2022-09-09 18:27:49 -0400

    refactorizacion del modelo

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,7 +9,7 @@ class Alumno(models.Model):
     edad = models.IntegerField(validators=[MinValueValidator(14), MaxValueValidator(20)])
     
     #Relationship
-    id_grado = models.ForeignKey("Grado", on_delete=models.CASCADE, null=True, blank=True)
+    id_aula = models.ForeignKey("Grado", on_delete=models.CASCADE, null=True, blank=True)
     
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
@@ -21,14 +21,13 @@ class Profesor(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     nombre = models.CharField(max_length=35)
     apellido = models.CharField(max_length=35)
-    edad = models.IntegerField(validators=[MinValueValidator(21), MaxValueValidator(60)])
-    numero_telefonico = models.CharField(max_length=35)
+    materia = models.CharField(max_length=35)
 
     is_active = models.BooleanField(default=True)
     
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
-    deleted_at = models.DateTimeField(null = True)
+    deleted_at = models.DateTimeField(null = True, blank=True)
     
     def __str__(self):
         return f"Docente: {self.nombre} {self.apellido} Numero Telefonico:{self.numero_telefonico}"
@@ -36,53 +35,24 @@ class Profesor(models.Model):
 class Aula(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     seccion = models.CharField(max_length=1)
-    capacidad = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(30)])
-    
-    #Relationship
-    id_grado = models.ForeignKey("Grado", on_delete=models.CASCADE, null=True, blank=True)
+    capacidad = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(30)])
     
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
-        return f"Seccion de aula: {self.seccion} Capacidad:{self.capacidad}/30"
-
-
-class Materia(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
-    nombre_materia = models.CharField(max_length=35)
-    
-    #Relationship
-    id_profesor = models.ForeignKey("Profesor", on_delete=models.CASCADE, null=True, blank=True)
-    id_grado = models.ForeignKey("Grado", on_delete=models.CASCADE, null=True, blank=True)
-    
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
-    
-    def __str__(self):
-        return f"Materia: {self.nombre_materia}"
-
-
-class Grado(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
-    grado = models.CharField(max_length=20)
-    
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
-    
-    def __str__(self):
-        return f"Grado: {self.grado}"
-
+        return f"Seccion de aula: {self.seccion} Capacidad:{self.capacidad}"
 
 class Turno(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
-    hora_de_inicio = models.TimeField()
-    hora_de_finalizacion = models.TimeField()
+    hora = models.TimeField()
     
     #Relationship
     id_profesor = models.ForeignKey("Profesor", on_delete=models.CASCADE, null=True, blank=True)
-    id_materia = models.ForeignKey("Materia", on_delete=models.CASCADE, null=True, blank=True)
     id_aula = models.ForeignKey("Aula", on_delete=models.CASCADE, null=True, blank=True)
     
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
+    
+    def __str__(self):
+        return f"Hora: {self.hora}"
