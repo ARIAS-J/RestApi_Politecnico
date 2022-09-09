9ba5d052a466549b86078ab0c929391d4270811b
No-Hex <evilheroe1@gmail.com>
2022-09-09 11:50:27 -0400

    update serializers

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -2,48 +2,60 @@ from rest_framework import serializers
 from .models import Alumno, Grado, Materia, Profesor, Aula, Turno
 
 class AlumnoSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+    updated_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
     class Meta:
         model=Alumno
-        fields='__all__'
+        fields=('id', 'matricula' 'nombre', 'apellido', 'edad', 'created_at', 'updated_at')
         
         # extra_kwargs = {}
 
 
 class ProfesorSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+    updated_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
     class Meta:
         model=Profesor
-        fields='__all__'
+        fields=('id', 'nombre', 'apellido', 'edad', 'numero_telefonico', 'is_active', 'created_at', 'updated_at', 'deleted_at')
         
         # extra_kwargs = {}
 
 
 class AulaSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+    updated_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
     class Meta:
         model=Aula
-        fields='__all__'
+        fields=('id', 'seccion', 'capacidad', 'id_grado', 'created_at', 'updated_at')
         
         # extra_kwargs = {}
 
 
 class MateriaSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+    updated_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
     class Meta:
         model=Materia
-        fields='__all__'
+        fields=('id', 'nombre_materia', 'id_profesor', 'created_at', 'updated_at')
         
         # extra_kwargs = {}
 
 
 class GradoSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+    updated_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
     class Meta:
         model=Grado
-        fields='__all__'
+        fields=('id', 'grado', 'id_materia', 'id_alumno', 'created_at', 'updated_at')
         
         # extra_kwargs = {}
 
 
 class TurnoSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+    updated_at = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
     class Meta:
         model=Turno
-        fields='__all__'
+        fields=('id', 'hora_de_inicio', 'hora_de_finalizacion', 'id_profesor', 'id_materia', 'id_aula', 'created_at', 'updated_at')
         
         # extra_kwargs = {}
