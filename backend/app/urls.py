4d15ae970fbb06cd775f19bd68f4e583991c13ee
No-Hex <evilheroe1@gmail.com>
2022-09-11 09:58:23 -0400

    rename path parameter and added token route

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import AlumnoList, AlumnoRetrieve, AsignarAula, ProfesorList, ProfesorRetrieve, AulaList, AulaRetrieve, TurnoList, TurnoRetrieve
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     # Alumnos Routes
     path('alumnos/', AlumnoList),
-    path('alumnos/<int:pk>', AlumnoRetrieve ),
+    path('alumnos/<int:alumno_id>', AlumnoRetrieve ),
     
     # Profesores Routes
     path('profesores/', ProfesorList),
@@ -13,13 +13,15 @@ urlpatterns = [
     
     # Aulas Routes
     path('aulas/', AulaList),
-    path('aulas/<int:pk>', AulaRetrieve),
+    path('aulas/<int:aula_id>', AulaRetrieve),
     
     # Turnos Routes
     path('turnos/', TurnoList),
-    path('turnos/<int:pk>', TurnoRetrieve),
+    path('turnos/<int:turno_id>', TurnoRetrieve),
     
     # Asignar
-    path('asignar_aula/<int:pk>', AsignarAula),
+    path('asignar_aula/<int:alumno_id>', AsignarAula),
     
+    # Obtain token route
+    path('token/', obtain_auth_token, name='obtain_token'),
 ]
